851a104bb55f006ca58a6182d85ad18d890ecd9a
kmeKame <68630209+kmeKame@users.noreply.github.com>
2023-08-03 22:55:17 -0300

    Update ATV07.php

--- ATV07.php
+++ ATV07.php
@@ -1,10 +1,24 @@
 <?php
+/* Salvei todos os 12 meses em uma lista e guardei ela
+em uma variável para ser usada mais tarde*/
+
+$mes = ["Janeiro", "Fevereiro", "Março",
+"Abril","Maio", "Junho", "Julho", "Agosto", "Setembro",
+"Outubro", "Novembro", "Dezembro"];
 
 $numMes = 8;
 
-if ($numMes <= 1 && $numMes >=12){
-echo "Mês é Inválido";
-}
-else echo "Agosto é um Mês Válido";
+if ($numMes >= 1 && $numMes <= 12) {
+  
+  /*a variavel armazena um número e esse número irá setar uma posição para ser lida, e
+  o mês que estiver nessa posição será mostrado na tela.*/
+  
+  echo $mes[$numMes -1];
 
+  /* a expressão -1 faz com que uma casa na posição seja subtraída, como a contagem começa em 0, sem
+  omenos 1 o mês seria sempre um mês a frente do que deveria ser relacionado*/
+}
+else {
+    echo "Mês Inválido.";
+}
 ?>
